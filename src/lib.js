b9d005dc517f7d4d689a67d6dca717c724ece822
Ed Armstrong <ed@frar.ca>
2023-04-13 14:45:34 -0400

    fix lib

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 import Logger from "./Logger.js";
 import colorize from "./colorize.js";
-import getPos, { position } from "./getPos.js";
+import position, { getPos } from "./getPos.js";
 
 export { Logger as default, colorize, getPos, position }
